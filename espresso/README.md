6526acdb180a584269d5d16912480e3b714f427f
Koan-Sin Tan <koansin.tan@gmail.com>
2023-01-14 20:50:35 +0800

    add a script to parse valid json net
    add a python script thaat can parse espress.net with valid json format

---
+++ espresso/README.md
@@ -0,0 +1,47 @@
+* get input/output tensor names and shapes from espresso.{net,shape}
+
+In modern macOS and iOS, most Core ML models are in *.espresso.{net,shape,weights}. Yes, there are some in mlmodelc format and 
+some are .hwx only, but they are relative few. For example, on macOS Ventura, we can find a scene detection network model
+related files described in Apple's "A Multi-Task Neural Architecture for On-Device Scene Analysis" article [1].
+
+```
+/System/Library/Frameworks/Vision.framework/Resources/SceneNet*
+```
+
+Checking a model's input/output tensor names and shapes could tell us somethings.
+It's seem quite obvious that the layer/op names and the shapes of layers of espresso models are in .espresso.{net,shape} files.
+The .espresso.{net,shape} files in iOS and macOS are in either json for lzfse [2] compressed json.
+
+Reading .json (either compressed or not) should be trivial. However, it seems some models are not stored in a consistent
+well-formated way, so we have to add some hacks.
+
+Before running the script, install lzfse python binding, `pip install pyliblzfse`
+
+Then we can do 
+```bash
+$ python  parse_net.py  /System/Library/Frameworks/Vision.framework/Resources/SceneNet_v5.10.0_vhh2692239_fe1.3_sc3.3_sa2.4_ae2.4_so2.4_od1.5_fp1.5.espresso.net 
+```
+and get
+```
+  input: image {'k': 3, 'w': 360, 'n': 1, '_rank': 4, 'h': 360}
+  output:  inner/sceneprint {'k': 768, 'w': 1, 'n': 1, '_rank': 4, 'h': 1}
+  output:  classification/labels {'k': 1374, 'w': 1, 'n': 1, '_rank': 4, 'h': 1}
+  output:  aesthetics/scores {'k': 2, 'w': 1, 'n': 1, '_rank': 4, 'h': 1}
+  output:  aesthetics/attributes {'k': 21, 'w': 1, 'n': 1, '_rank': 4, 'h': 1}
+  output:  detection/scores {'k': 30, 'w': 90, 'n': 1, '_rank': 4, 'h': 90}
+  output:  detection/coordinates {'k': 4, 'w': 90, 'n': 1, '_rank': 4, 'h': 90}
+  output:  fingerprint/embedding {'k': 4, 'w': 6, 'n': 1, '_rank': 4, 'h': 6}
+  output:  objectness/map {'k': 1, 'w': 68, 'n': 1, '_rank': 4, 'h': 68}
+  output:  saliency/map {'k': 1, 'w': 68, 'n': 1, '_rank': 4, 'h': 68}
+
+```
+
+Surely, we can do
+```
+for m in /System/Library/Frameworks/Vision.framework/Resources/*.espresso.net;do python parse_net.py $m ;done
+```
+to dump all the `Vision.framework` models.
+
+[1] https://machinelearning.apple.com/research/on-device-scene-analysis
+[2] https://en.wikipedia.org/wiki/LZFSE
+
---
+++ espresso/parse_net.py
@@ -0,0 +1,112 @@
+import json
+import sys
+
+import liblzfse
+
+def read_espresso_file(file):
+    compressed=False
+    with open(file, 'rb') as f:
+        first_4_bytes = f.peek(4)[:4]
+        if (first_4_bytes == b'pbze'):
+            compressed = True
+        if compressed:
+            f.seek(28)
+            compressed_buffer = f.read()
+            json_data = liblzfse.decompress(compressed_buffer)
+        else:
+            json_data = f.read()
+    return json_data
+
+NET_FILE=sys.argv[1]
+
+net_buffer = read_espresso_file(NET_FILE)
+net_dict = json.loads(net_buffer)
+
+shape_buffer = read_espresso_file(NET_FILE.replace('espresso.net', 'espresso.shape'))
+shape_dict = json.loads(shape_buffer)
+
+# take .espresso.net file
+print(NET_FILE)
+
+class Node:
+    def __init__(self, name):
+        self.name = name
+        self.bottoms = []
+        self.tops = []
+        self.type = None
+        self.is_output = 0
+
+nodes = {}
+for n in net_dict['layers']:
+    k = n['name']
+    if not k in nodes:
+        nodes[k] = Node(k)
+    nodes[k].type = n['type']
+   
+    # if a layer is explicitly annotated as 'is_output', we take it 
+    if ('attributes' in n) and ('is_output' in n['attributes']):
+        nodes[k].is_output = n['attributes']['is_output']
+   
+    # connecting with 'bottom' layers 
+    if not n['bottom'] == '':
+        bottoms = n['bottom'].split(',')
+        for b in bottoms:
+            if not b in nodes:
+                nodes[b] = Node(b)
+                #print("adding", b)
+            # dont layers with is_output == 1 as bottom layers
+            if not (nodes[b].is_output == 1):
+              nodes[k].bottoms.append(b)
+              nodes[b].tops.append(k)
+            #print(b)
+
+    # connecting with 'top' layers 
+    if not n['top'] == '':
+        tops = n['top'].split(',')
+        for t in tops:
+            if not t in nodes:
+                nodes[t] = Node(t)
+                #print("adding", t)
+            # ignore layers with top == itself
+            if not (k == t):
+              nodes[k].tops.append(t)
+              nodes[t].bottoms.append(k)
+            #print("t.tops:", nodes[t].name, nodes[t].tops)
+            
+output_dict = {}
+# print(len(net_dict['layers']))
+for n in nodes:
+    #print(nodes[n].name)
+    #print(" tops:", nodes[n].tops)
+    #print(" bottoms:", nodes[n].bottoms)A
+
+    if (len(nodes[n].bottoms) == 0) and (nodes[n].type == None):
+        shape = shape_dict['layer_shapes'][n]
+        print("  input:", n, shape)
+
+    if len(nodes[n].tops) == 0 or (nodes[n].is_output == 1):
+        if len(nodes[n].tops) == 0: # and (nodes[n].is_output == 1):
+          if not (n in output_dict):
+            shape = shape_dict['layer_shapes'][n]
+            print("  output: ", n, shape)
+            output_dict[n] = shape
+        else:
+          if not (nodes[n].tops[0] in output_dict):
+            shape = shape_dict['layer_shapes'][nodes[n].tops[0]]
+            print("  output: ", nodes[n].tops[0], shape)
+            output_dict[nodes[n].tops[0]] = shape
+        #elif len(nodes[n].tops) == 0 and (nodes[n].is_output == 0):
+        #  if not (n in output_dict):
+        #    shape = shape_dict['layer_shapes'][n]
+        #    print("  output: ", n, shape)
+        #    output_dict[n] = shape
+
+        #if (n not in shape_dict['layer_shapes']):
+        #  shape = shape_dict['layer_shapes'][nodes[n].bottoms[0]]
+          # print("  output: ", nodes[n].bottoms[0], shape)
+        #  print("  output: ", nodes[n].tops[0], shape)
+          #pass
+        #else:
+        #  if len(nodes[n].tops) == 0 and (not (nodes[nodes[n].bottoms[0]].is_output == 1)):
+        #    shape = shape_dict['layer_shapes'][n]
+        #    print("  output: ", n, shape)
